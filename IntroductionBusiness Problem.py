1df254d3de19c07d283399a7556f0e35ad53ae29
Alvy001 <71112497+Alvy001@users.noreply.github.com>
2020-11-07 18:09:30 +1100

    Add files via upload

---
+++ IntroductionBusiness Problem.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project — The Battle of Neighbourhoods
+# 
+# ## **Introduction**
+# 
+# **Noida**, short for **New Okhla Industrial Development Authority**, is a planned city located in Gautam Buddh Nagar district of the Indian state of Uttar Pradesh. It is a satellite city of Delhi and is a part of the National Capital Region of India. As per provisional reports of Census of India, the population of Noida in 2011 was 642,381. 
+# The official language of Noida and the one that is most widely spoken is Hindi.
+# With its diverse culture, comes diverse food items. There are many restaurants in Noida, each belonging to different categories like Chinese, Italian, French etc. So as part of this project, we will list and visualise all major parts of Noida.
+# 
+# **Questions that can be asked using the above-mentioned datasets**
+# 
+#  - Which places In Noida have some of the finest Restaurants?
+#  - Which Places in Noida have Restaurants with Lowest rating?
+#  - Which place in Noida is suitable for a Foodie?
+#  - Which place in Noida is not suitable for a Foodie?
+#  - What are the best places in Noida for Chinese Restaurant?
+#  - Which places in Noida have highest rated Chinese Restaurants?
+
+# In[ ]:
+
+
+
+
