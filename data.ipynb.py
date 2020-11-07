f2365bbfc1583df46d98043b5855434c6c016867
Alvy001 <71112497+Alvy001@users.noreply.github.com>
2020-11-07 18:12:22 +1100

    Add files via upload

---
+++ data.ipynb.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## **Data**
+# 
+# For this project we need the following data:
+# 
+# - Noida Restaurants data that contains list Locality, Restaurant name, Rating along with their  latitude and longitude.	
+#      - Data Source: [zomatoKaggle](https://www.kaggle.com/shrutimehta/zomato-restaurants-data)
+#      - Description: This data set contains the required information which we will use to explore various localities of Noida city.
+# - Nearby places in each locality of Noida city.
+#     - Data Source: [FourSquareAPI](https://developer.foursquare.com)
+#     - Description: By using this API we will get all the venues in each neighbourhood.
+
+# In[ ]:
+
+
+
+
